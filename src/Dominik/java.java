b1301d500fa890cf552b6930e47afbefc420532f
Dominik Wator <watord@sheridancollege.ca>
2023-06-23 10:00:17 -0400

    Im Done

---
+++ src/Dominik/java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dominik;
+
+/**
+ *
+ * @author dominikwator
+ */
+public class java {
+    
+}
+
+//This is my new java class
+//Dominik.java
